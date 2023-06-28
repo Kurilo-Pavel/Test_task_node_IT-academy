777011db78ea0d57e3a9b1a03bc8e714d0ffc6f6
Pavel <kurilo.pavel@mail.ru>
2023-06-28 22:22:03 +0300

    Send file xml, json, html

--- Task_1/server.js
+++ Task_1/server.js
@@ -20,6 +20,7 @@ const writeLog = (logFilePath, logLine) => {
 const users = () => {
   return fs.readFileSync(pathFile, "utf8");
 };
+let currentUsers = users();
 
 server.use(express.json());
 
@@ -34,7 +35,7 @@ server.get('/variants', (request, response) => {
   }));
 });
 
-server.get('/stat', (request, response) => {
+server.post('/stat', (request, response) => {
   writeLog(pathLog, `[${port}]` + ' ' + 'stat called');
   response.send(JSON.parse(users()).map(user => {
     return {id: user.id, votes: user.votes};
@@ -43,19 +44,54 @@ server.get('/stat', (request, response) => {
 
 server.post('/vote', (request, response) => {
   writeLog(pathLog, `[${port}]` + ' ' + 'vote called');
-  const currentUsers = JSON.parse(users()).map(user => {
+  currentUsers = JSON.stringify(JSON.parse(users()).map(user => {
     if (user.id === request.body.id) {
       user.votes++;
     }
     return user;
-  });
-  fs.writeFile(pathFile, JSON.stringify(currentUsers), (err) => {
+  }));
+  fs.writeFile(pathFile, currentUsers, (err) => {
     if (err) {
       console.log(err);
     }
   });
   response.send(request.body);
 })
+
+
+server.get('/download', (request, response) => {
+  const fileType = request.headers["content-type"];
+  let data = '';
+  switch (fileType) {
+    case 'application/xml':
+      writeLog(pathLog, `[${port}]` + ' ' + 'send file xml');
+      const fileXML = fs.createWriteStream('voice.xml');
+      JSON.parse(currentUsers).forEach(user => {
+        data += `<User><name>${user.name}</name><votes>${user.votes}</votes></User>` + os.EOL;
+      });
+      fileXML.write(`<Users>${os.EOL}${data}</Users>`, () => {
+        response.sendFile(__dirname + '/voice.xml');
+      });
+      break;
+    case 'application/json':
+      writeLog(pathLog, `[${port}]` + ' ' + 'send file json');
+      const fileJSON = fs.createWriteStream('voice.json');
+      fileJSON.write(currentUsers, () => {
+        response.sendFile(__dirname + '/voice.json');
+      });
+      break;
+    case 'text/html':
+      writeLog(pathLog, `[${port}]` + ' ' + 'send file html');
+      const fileHTML = fs.createWriteStream('voice.html', {flags: 'w'});
+      JSON.parse(currentUsers).forEach(user => {
+        data += `<div><span>${user.name}</span> - <span>${user.votes}</span></div>` + os.EOL;
+      });
+      fileHTML.write(`<div>${os.EOL}${data}</div>`, () => {
+        response.sendFile(__dirname + '/voice.html');
+      });
+      break;
+  }
+});
 
 server.listen(port, () => {
   console.log("server start");
