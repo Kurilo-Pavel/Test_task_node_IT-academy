2ec0aa8590675da67a036865c23172f0d501b756
Pavel <kurilo.pavel@mail.ru>
2023-07-19 19:54:15 +0300

    fix mistake

--- Task_4/server.js
+++ Task_4/server.js
@@ -11,19 +11,19 @@ const pathLog = path.join(__dirname, '/server.log');
 
 server.use(express.urlencoded({extended: true}));
 
-const url = `http://localhost:7781`;
-// const url=`http://178.172.195.18:7781`;
+// const url = `http://localhost:7781`;
+const url=`http://178.172.195.18:7781`;
 
 server.engine("hbs", expressHbs.engine(
   {
-    layoutsDir: "Task_4/views/layouts",
+    layoutsDir: `${__dirname}/views/layouts`,
     defaultLayout: "layout",
     extname: "hbs"
   }
 ));
 
 server.set("view engine", "hbs");
-server.set("views", "Task_4/views");
+server.set("views", `${__dirname}/views`);
 hbs.registerPartials(__dirname + "/views/partials");
 
 const writeLog = (logFilePath, logLine) => {
@@ -37,24 +37,38 @@ const writeLog = (logFilePath, logLine) => {
 
 server.get('/', (req, res) => {
   writeLog(pathLog, `[${port}]` + ' ' + 'form called');
-  res.send(
-    `<form method="post" action=${url}>` +
-    '<label for="name"></label>' +
-    `<input type="text" id="name"  name="name" value=${req.query.name === undefined ? '' : req.query.name} >` +
-    '</br>' +
-    '<label for="password"></label>' +
-    `<input type="text" id="password" name="password" value=${req.query.password === undefined ? '' : req.query.password} >` +
-    '</br>' +
-    '<button type="submit">Send</button>' +
-    '</form>'
-  );
+  if (req.query.name && req.query.password) {
+    res.send(`<h1>Your name - ${req.query.name} and password - ${req.query.password}</h1>`);
+  } else {
+    let name = '';
+    let password = '';
+    if (req.query.name !== undefined && req.query.name.length === 0) {
+      name = 'You should enter name';
+    }
+    if (req.query.password !== undefined && req.query.password.length === 0) {
+      password = 'You should enter password';
+    }
+    res.send(
+      '<form method="post">' +
+      '<label for="name"></label>' +
+      `<input type="text" id="name"  name="name" value=${req.query.name === undefined ? '' : req.query.name} >` +
+      `<span>${name}</span>` +
+      '</br>' +
+      '<label for="password"></label>' +
+      `<input type="text" id="password" name="password" value=${req.query.password === undefined ? '' : req.query.password} >` +
+      `<span>${password}</span>` +
+      '</br>' +
+      '<button type="submit">Send</button>' +
+      '</form>'
+    );
+  }
 });
 
 server.post("/", (req, res) => {
   if (req.body.name && req.body.password) {
-    res.redirect(301, `/user?name=${req.body.name}&password=${req.body.password}`);
-  } else {
-    res.redirect(301, "/notfound");
+    res.redirect(301, `${url}/user?name=${req.body.name}&password=${req.body.password}`);
+  }else{
+    res.redirect(301, `${url}/?name=${req.body.name}&password=${req.body.password}`)
   }
 });
 
@@ -62,10 +76,6 @@ server.get("/user", (req, res) => {
   res.render("index", {name: req.query.name, password: req.query.password});
 });
 
-server.get("/notfound", (req, res) => {
-  res.status(404);
-  res.render("notFound.hbs");
-});
 
 server.listen(port, () => {
   console.log("server start work Task 4");
